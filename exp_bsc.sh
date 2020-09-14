f20c57e5bde0dfcf723cd753a00e79ee5db7cb2b
pdmorale <pabloa.morale@gmail.com>
2020-09-14 22:23:14 +0900

    modified exp_bsc

--- exp_bsc.sh
+++ exp_bsc.sh
@@ -12,24 +12,27 @@ fi
 target_dir=$1
 echo "($# args) Directory: $target_dir"
 
-for entry in "$target_dir"/*
+for entry in "$target_dir"/*.csv
 # rename files to introduce some regularity
+# in this example we consider csv files (amount up to 2 digits)
 counter=1
 do
-	mv $entry "file $i"
+	mv $entry "file "$(printf "%2d" $counter)
 	counter=counter+1
 done
 
-exit 1
+#exit 1
 
-for i in {23..24}
+for i in 0{0..9} {10..24}
 do
-	target="$target_dir/file $i"
-	#echo $target
-	sed -i '' 's|target_dir|'"$target"'|g' convert2csv.m
-	$PATH_TOMAT -nodisplay -nosplash -nodesktop -r "run(./convert2csv.m);exit;"
-	echo "gotta move those $i son!"
-	#mv SS"$i"* /Users/pablomorales/Desktop/FB2L/Data/DTCPA_Data/SS"$i"
-	echo "resetting .m file"
-	sed -i '' 's|'"$target"'|target_dir|g' convert2csv.m
+	target="$target_dir/dir$i"
+	if [ -d "$target" ] then;
+		#echo $target
+		sed -i '' 's|target_dir|'"$target"'|g' convert2csv.m
+		$PATH_TOMAT -nodisplay -nosplash -nodesktop -r "run(./convert2csv.m);exit;"
+		echo "gotta move those $i son!"
+		#mv SS"$i"* /Users/pablomorales/Desktop/FB2L/Data/DTCPA_Data/SS"$i"
+		echo "resetting .m file"
+		sed -i '' 's|'"$target"'|target_dir|g' convert2csv.m
+	fi
 done
